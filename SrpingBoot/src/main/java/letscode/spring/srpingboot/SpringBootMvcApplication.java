068f92b29bf1431773eab387e4cd11bb0149c48b
reqxar <reqxar@gmail.com>
2023-10-08 00:52:27 +0300

    Initialize SpringBootMvcApplication

--- SrpingBoot/src/main/java/letscode/spring/srpingboot/SpringBootMvcApplication.java
+++ SrpingBoot/src/main/java/letscode/spring/srpingboot/SpringBootMvcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SrpingBootApplication {
+public class SpringBootMvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SrpingBootApplication.class, args);
+        SpringApplication.run(SpringBootMvcApplication.class, args);
     }
 
 }
